3458d9fe5aac3a7a08162ea247fd5f8fdc5dd683
jebalimahjoub <mahjoub_jebali@yahoo.fr>
2023-02-20 15:00:20 +0100

    Create Angular component for product-list

--- eCommerceSite/src/app/app.module.ts
+++ eCommerceSite/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProduitListComponent } from './components/produit-list/produit-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProduitListComponent
   ],
   imports: [
     BrowserModule,
