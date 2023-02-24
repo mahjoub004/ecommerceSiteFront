733130817605656a4ec695a009d00efb9e28997a
jebalimahjoub <mahjoub_jebali@yahoo.fr>
2023-02-24 10:28:10 +0100

    add template HTML/CSS and cut this project in components

--- eCommerceSite/src/app/app.module.ts
+++ eCommerceSite/src/app/app.module.ts
@@ -6,10 +6,18 @@ import { AppComponent } from './app.component';
 import { ProduitListComponent } from './components/produit-list/produit-list.component';
 import { HttpClientModule } from "@angular/common/http";
 import { ProduitService } from './services/produit.service';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { FeaturesSectionComponent } from './components/features-section/features-section.component';
+import { SliderComponent } from './components/slider/slider.component';
 @NgModule({
   declarations: [
     AppComponent,
-    ProduitListComponent
+    ProduitListComponent,
+    FooterComponent,
+    NavBarComponent,
+    FeaturesSectionComponent,
+    SliderComponent
   ],
   imports: [
     BrowserModule,
